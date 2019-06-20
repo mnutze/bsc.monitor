a016d9f91a2320c666846db144d34e2346ac067d
Michael Nutzenberger <michael.nutzenberger@smail.inf.h-brs.de>
2019-06-20 10:55:48 +0200

    Monitor "Latest Activities": made it subject ready

--- assets/monitor.latest.js
+++ assets/monitor.latest.js
@@ -8,6 +8,19 @@ ccm.files["monitor.latest.js"] = function (data, instance) {
     if (data.length < 1)
         return;
 
+    if (Object.keys(instance.filter).length === 0 )
+        instance.filter = { and: [ {} ] };
+
+    let subjectFilter;
+    if (instance.subject && instance.subject.values.length > 0)
+        subjectFilter = instance.subject.values.map(subject => ({ "===" : [ { var : instance.subject.key }, subject ] }) );
+
+    if (subjectFilter)
+        subjectFilter.forEach(filter => {
+            if (!instance.filter.and.includes(filter))
+                instance.filter.and.push(filter);
+        });
+
     // filter data older than "value" minutes
     data = helper.datetime.gt(data, instance.range.value);
 
